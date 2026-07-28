66f6efc1c3dca6c0290a96cec2d4c2f34e93e012
Mykola Mokhnach <mokhnach@gmail.com>
2026-07-28 19:14:47 +0200

    chore: upgrade react-native to 0.86.2 (#151)

---
+++ ios/VodQAReactNative/SceneDelegate.swift
@@ -0,0 +1,28 @@
+import UIKit
+import React
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = scene as? UIWindowScene,
+      let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+      let factory = appDelegate.reactNativeFactory
+    else {
+      return
+    }
+
+    let window = UIWindow(windowScene: windowScene)
+    self.window = window
+
+    factory.startReactNative(
+      withModuleName: "VodQAReactNative",
+      in: window,
+      launchOptions: nil
+    )
+  }
+}
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-native-community|@react-navigation|react-native-screens|react-native-gesture-handler|react-native-safe-area-context|react-native-image-viewing|react-native-webview)/)',
